224a9737055ffaa39d6c86e339e90e0487bbf9fb
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-13 17:46:46 +0300

    First route created for functionality testing.
    Starting home page design.
    
    Will protect routes later on..
    
    With nodejs connection.
    
    Keep going.
    
    Remmeber why you started.

---
+++ pages/cardio.tsx
@@ -0,0 +1,11 @@
+import c from "../styles/cardio.module.css";
+
+const Cardio = () => {
+    return ( 
+        <div className={c.cardio}>
+            Cardio goes here...
+        </div>
+     );
+}
+ 
+export default Cardio;
