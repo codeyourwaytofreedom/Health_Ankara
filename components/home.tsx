caeaf736872dd6fc443b795082864fc7e479a611
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-13 16:24:58 +0300

    new project start

---
+++ components/home.tsx
@@ -0,0 +1,11 @@
+import h from "../styles/Home.module.css";
+
+const Home_p = () => {
+    return (
+        <div className={h.home}>
+                Hello New Project
+        </div>
+      );
+}
+ 
+export default Home_p;
