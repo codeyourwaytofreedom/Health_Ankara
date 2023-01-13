c9eb76a62286cd76d1fe2b9bb655e3d38cb87e1f
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-13 22:17:40 +0300

    Homepage skeleton design in place.
    Will add navigating buttons around the skeleton..
    
    according to human anatomy.
    
    Will look pretty.
    
    Keep going.
    
    Remember why you started.

--- components/home.tsx
+++ components/home.tsx
@@ -1,9 +1,17 @@
 import h from "../styles/Home.module.css";
+import Image from "next/image";
 
 const Home_p = () => {
     return (
         <div className={h.home}>
-                Hello New Project
+            <div className={h.home_skeleton}>
+<                   Image
+                    src="/heart.png"
+                    width={90}
+                    height={130}
+                     />
+            </div>
+                
         </div>
       );
 }
