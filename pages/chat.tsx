cc8dd50832197ce3f3321c1392d15995ad894fe6
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-14 16:27:46 +0300

    First connection test with socket worked.
    Will add on it.
    
    Will test msg first.
    
    Keep going.
    
    Remember why you started.

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -3,51 +3,43 @@ import io from 'socket.io-client';
 import { useState } from "react";
 
 
-const socket = io("http://localhost:9000")
+const socket = io.connect("http://localhost:9000")
 
 
 const Chat = () => {
-    const [price_from_socket, setPricefromScoket] = useState(null);
-
+    const [isConnected, setIsConnected] = useState(socket.connected);
+    const [lastPong, setLastPong] = useState("p");
+  
     useEffect(() => {
-        socket.on('connect', () => {
-          console.log("Connected to Socket")
-        });
-    
-        socket.on('disconnect', () => {
-            console.log("Disconnected from Socket")
-        });
-
-        socket.on('chat message', (ms) => {
-            console.log("New user message received from the Socket server")
-        });
-
-        socket.on('pong', () => {
-            console.log("Ponged Socket")
-        });
-    
-        return () => {
-          socket.off('connect');
-          socket.off('disconnect');
-          socket.off('pong');
-        };
-      }, []);
-
-
-
-
-    const price = 500;
-    const bidup = () => {
-        socket.emit('chat message', price_from_socket ?? price);
+      socket.on('connect', () => {
+        setIsConnected(true);
+      });
+  
+      socket.on('disconnect', () => {
+        setIsConnected(false);
+      });
+  
+      socket.on('pong', () => {
+        setLastPong("ponged");
+      });
+  
+      return () => {
+        socket.off('connect');
+        socket.off('disconnect');
+        socket.off('pong');
+      };
+    }, []);
+  
+    const sendPing = () => {
+      socket.emit('ping');
     }
 
     return ( 
-        <div className="live">
-            <div className="live_images">
-                { price_from_socket ?? price}
-                <button>Test Socket</button>
-            </div>
-        </div> 
+        <div>
+        <p>Connected: { isConnected }</p>
+        <p>Last pong: { lastPong }</p>
+        <button onClick={ sendPing }>Send ping</button>
+      </div>
     );
 }
  
