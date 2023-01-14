62f1e73b69cfa01e303990edaac97bf8241968e7
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-14 13:46:15 +0300

    First logic connection in place.
    Link put in place to navigate to cardio page.
    
    Already looks pretty.
    
    Keep going.
    
    Remember why you started.

--- components/home.tsx
+++ components/home.tsx
@@ -1,15 +1,22 @@
 import h from "../styles/Home.module.css";
 import Image from "next/image";
+import Link from 'next/link';
 
 const Home_p = () => {
     return (
         <div className={h.home}>
             <div className={h.home_skeleton}>
-<                   Image
-                    src="/heart.png"
-                    width={90}
-                    height={130}
-                     />
+                <div className={h.home_skeleton_heart}>
+                    <Link href="/cardio">
+                        <Image
+                        src="/heart.png"
+                        width={60}
+                        height={100}
+                        />
+                    </Link>
+                </div>
+                
+                    
             </div>
                 
         </div>
