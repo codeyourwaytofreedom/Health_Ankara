824368b83ee64f80303477c03f5d1a4ddbc34084
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-16 18:04:25 +0300

    WhatsApp like chat screen done.
    Desk vs Customer message designs in place.
    
    Chatbox design 90% done.
    
    Great progress today.
    
    Keep going.
    
    Remember why you started.

--- components/chat.tsx
+++ components/chat.tsx
@@ -9,55 +9,38 @@ const socket = io.connect("http://localhost:9000")
 
 
 const Chat = ({modal}) => {
-    const [my_messages, setMymessages] = useState([]);
-    
-    
-    socket.on('answer', answer => {
-      setMymessages([...my_messages, {who:"desk", message:answer}])
-    });
-
-
-/*     useEffect(() => {
+  const [my_messages, setMymessages] = useState([]);
+  const customer = useRef();
   
-      socket.on('disconnect', () => {
-      });
-  
-      socket.on('chat message', msg => {
-      });
+  socket.on('answer', answer => {
+    setMymessages([...my_messages, {who:"desk", message:answer}])
+  });
 
-  
-      return () => {
-        socket.off('connect');
-        socket.off('disconnect');
-        socket.off('pong');
-      };
-    }, []);
-     */
-    const customer = useRef();
-
-
-    const handle_customer = (e) => {
-      e.preventDefault();
-      setMymessages([...my_messages, {who:"customer", message:customer.current.value}])
-      console.log(my_messages)
-      socket.connect();
-      socket.emit('customer-asking', customer.current.value);
-      customer.current.value = "";
-    }
+  const handle_customer = (e) => {
+    e.preventDefault();
+    setMymessages([...my_messages, {who:"customer", message:customer.current.value}])
+    console.log(my_messages)
+    socket.connect();
+    socket.emit('customer-asking', customer.current.value);
+    customer.current.value = "";
+  }
 
     return ( 
 
           <div className={h.home_chatbox} style={{display: modal ? "flex" : "none"}}>
-            {
-              my_messages.map((m) => 
-                <div className={h.home_chatbox_message}>
-                  {m.message}
-                </div>
-              )
-            }
+            <div className={h.home_chatbox_message_container}>
+              {
+                my_messages.map((m) => 
+                  <div className={m.who === "desk" ? h.home_chatbox_message_container_message_right : h.home_chatbox_message_container_message_left}>
+                    {m.message}
+                  </div>
+                )
+              }
+            </div>
+            
             <div className={h.home_chatbox_sender}>
               <form action="" onSubmit={(e)=> handle_customer(e) }>
-                <input type="text" ref={customer} />
+                <input type="text" placeholder="Ask help desk..." ref={customer} />
               </form>
             </div>
           </div>
