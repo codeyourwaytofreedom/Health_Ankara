4a70542f21aaf1b285155e3687a7a0c99490fdc9
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-16 15:16:41 +0300

    Problem solved.
    Desk answers no longer overwrites the messages array.
    
    Great step forward.
    
    Next step: Prettify the chatbox.
    
    Keep going.
    
    Remmeber why you started.

--- components/chat.tsx
+++ components/chat.tsx
@@ -5,27 +5,24 @@ import h from "../styles/Home.module.css";
 import e from "cors";
 
 
-const socket = io.connect("http://localhost:9000", { autoConnect: false })
+const socket = io.connect("http://localhost:9000")
 
 
 const Chat = ({modal}) => {
-    const [fb, setFB] = useState("")
-  
-    useEffect(() => {
-      socket.on('connect', () => {
-        //console.log(socket.id)
-        //setFB(socket.id)
-      });
+    const [my_messages, setMymessages] = useState([]);
+    
+    
+    socket.on('answer', answer => {
+      setMymessages([...my_messages, {who:"desk", message:answer}])
+    });
+
+
+/*     useEffect(() => {
   
       socket.on('disconnect', () => {
       });
   
       socket.on('chat message', msg => {
-      });
-
-      socket.on('answer', answer => {
-        console.log(answer)
-        setFB(answer)
       });
 
   
@@ -35,28 +32,34 @@ const Chat = ({modal}) => {
         socket.off('pong');
       };
     }, []);
-    
+     */
     const customer = useRef();
 
 
     const handle_customer = (e) => {
       e.preventDefault();
-      console.log("sent message")
+      setMymessages([...my_messages, {who:"customer", message:customer.current.value}])
+      console.log(my_messages)
       socket.connect();
       socket.emit('customer-asking', customer.current.value);
+      customer.current.value = "";
     }
 
     return ( 
 
-          <div className={h.home_chatbox} style={{display: modal ? "grid" : "none"}}>
-            <div>
-              <form action="">
+          <div className={h.home_chatbox} style={{display: modal ? "flex" : "none"}}>
+            {
+              my_messages.map((m) => 
+                <div className={h.home_chatbox_message}>
+                  {m.message}
+                </div>
+              )
+            }
+            <div className={h.home_chatbox_sender}>
+              <form action="" onSubmit={(e)=> handle_customer(e) }>
                 <input type="text" ref={customer} />
-                <br />
-                <button type="submit" onClick={ (e)=> handle_customer(e) }>Customer</button>
               </form>
             </div>
-              
           </div>
     );
 }
