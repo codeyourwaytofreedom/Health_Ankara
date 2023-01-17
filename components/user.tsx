f556059cc1b9034606671828aa92fb5ceef22ef4
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-17 15:07:35 +0300

    Multiple Chat Boxes created in Desk page.
    Assigns id to each to filter messages.
    
    A small bug to be removed.
    
    Keep going.
    
    Remmeber why you started.

---
+++ components/user.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import d from "../styles/desk.module.css";
+import io from 'socket.io-client';
+
+const socket = io.connect("http://localhost:9000")
+
+const User = ({uniq}) => {
+    const [user_id, setUserid] = useState();
+    const [messages, setMessages] = useState([]);
+
+    useEffect(() => {
+        socket.on('connect', () => {
+        });
+    
+        socket.on('disconnect', () => {
+        });
+
+        socket.on('receive-question', (q, id) => {
+            console.log(id)
+            console.log(uniq)
+            if(uniq === id)
+            {
+                setUserid(id)
+                setMessages([...messages, q])
+            }
+          });
+      });
+
+    return ( 
+        <div className={d.desk_actives_user}>
+            <h3>{uniq}</h3>
+            {
+                messages.map((m) => 
+                  <div>
+                    {m}
+                  </div>
+                )
+              }
+        </div>
+     );
+}
+ 
+export default User;
--- pages/desk.tsx
+++ pages/desk.tsx
@@ -3,6 +3,8 @@ import { useEffect, useRef } from "react";
 import io from 'socket.io-client';
 import { useState } from "react";
 import { Socket } from "socket.io";
+import User from "../components/user";
+import Chat from "../components/chat";
 
 
 const socket = io.connect("http://localhost:9000")
@@ -22,10 +24,11 @@ const Desk = () => {
         });
 
         socket.on('receive-question', (q, id) => {
-            console.log(q)
-            console.log(id)
             setAnswer(id)
-            setOnline_users([...online_users, {user:id, question:q}])
+            if(!online_users.includes(id))
+            {
+                setOnline_users([...online_users, id])
+            }
           });
       });
 
@@ -38,15 +41,14 @@ const Desk = () => {
     return ( 
         <div className={d.desk}>
             <div className={d.desk_actives}>
-                {
-                    online_users.map(u =>
-                        <div className={d.desk_actives_user}>
-                            Active User
-                        </div>
-                    )
-                }
-                
+            {
+                online_users.map(u =>
+                    <User uniq={u}/> 
+                )
+            }
             </div>
+            
+            
             <div>
                 <h1>{answer}</h1>
                 <br />
