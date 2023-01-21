fee9fcbd6df5a18376d5fd95ad919946559e80cf
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-21 14:17:20 +0300

    Two different chatbox background in place.
    If the final message is from the user,
    chatbox is red to alert the desk officer...
    to show that that specific user is waiting for an answer.
    
    Great addition to the help desk system.
    
    Keep going.
    
    Remember why you started.

--- components/user.tsx
+++ components/user.tsx
@@ -3,25 +3,23 @@ import d from "../styles/desk.module.css";
 import io from 'socket.io-client';
 import h from "../styles/Home.module.css";
 import bg from "../public/background.jpg";
-
+import redbg from "../public/redbg.jpg";
 const socket = io.connect("http://localhost:9000")
 
 const User = ({uniq, user_messages}) => {
     const [user_id, setUserid] = useState();
     const ans = useRef(); 
-    const [messages_in_desk_window, setMessagesinDesk] = useState([]);
 
     const handle_desk = (e) => {
       e.preventDefault();
       socket.connect()
       socket.emit("answer",{to:uniq,text:ans.current.value})
-      setMessagesinDesk([...messages_in_desk_window, ans.current.value])
       ans.current.value = "";
     }
     
     return ( 
       <>
-        <div className={d.desk_actives_user} style={{ backgroundImage: `url(${bg.src})` }}>
+        <div className={d.desk_actives_user} style={{ backgroundImage: user_messages[user_messages.length-1].q ? `url(${redbg.src})` : `url(${bg.src})` }}> 
             <h3 style={{color:"white"}}>{uniq}</h3>
             <div className={d.desk_actives_user_container}>
               {
@@ -38,7 +36,6 @@ const User = ({uniq, user_messages}) => {
                 <input type="text" ref={ans}/>
               </form>
             </div>
-            
         </div>
       </>
      );
