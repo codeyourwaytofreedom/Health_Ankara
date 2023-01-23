262d5e012c705d4df5530b11618f222f5ada4e33
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-23 20:43:09 +0300

    Navbar done.
    Service clauses imported from a seperate js file.
    
    Home page design 90% done.
    
    Updated component positions.
    
    Will add happ smiles section.
    
    Keep going.
    
    Remember why you started.

--- components/home.tsx
+++ components/home.tsx
@@ -6,10 +6,12 @@ import Chat from "./chat";
 import Navbar from "./navbar";
 import Blinking_title from "./blinking_title";
 import Clause from "./clause";
+import {braces} from "./texts.js";
 
 const Home_p = () => {
     const [modal, setModal] = useState(false)
-    const braces = "The purpose of orthodontic treatment is to make the best of your teeth. This includes straightening your teeth so you're able to care for your teeth and gums more easily, and improving your bite so you can eat more comfortably. And your smile will benefit, too. Treatment almost always involves using braces to straighten crooked, crowded or protruding teeth, close gaps between teeth, and correct the bite so the top and bottom teeth meet when the mouth is closed. You'll need to have healthy teeth and gums before you can have a brace fitted. This is because you must be able to keep your teeth and your brace very clean while you're wearing it to avoid getting tooth decay or gum disease. Treatment usually lasts from 12 months to 2 and a half years, and visits to the orthodontist are needed every 4 to 10 weeks."
+    const [service, setService] = useState(braces)
+
     return (
         <div className={h.home}>
             <div className={h.home_skeleton}>
@@ -33,7 +35,11 @@ const Home_p = () => {
                 </span> */}
             </div>
             <div className={h.home_servicedetails}>
-                <Clause content={"The purpose of orthodontic treatment is to make the best of your teeth."}/>
+                {
+                    service.map(c=>
+                    c
+                    )
+                }
             </div>
             <Chat modal={modal}/>
         </div>
---
+++ components/texts.js
@@ -0,0 +1,19 @@
+import Clause from "./clause";
+
+export const braces = [
+    <Clause content={"The purpose of orthodontic treatment is to make the best of your teeth."}/>,
+    <Clause content={"This includes straightening your teeth so you're able to care for your teeth and gums more easily, and improving your bite so you can eat more comfortably. And your smile will benefit, too."}/>,
+    <Clause content={"Treatment almost always involves using braces to straighten crooked, crowded or protruding teeth, close gaps between teeth, and correct the bite so the top and bottom teeth meet when the mouth is closed."}/>,
+    <Clause content={"You'll need to have healthy teeth and gums before you can have a brace fitted."}/>,
+    <Clause content={"This is because you must be able to keep your teeth and your brace very clean while you're wearing it to avoid getting tooth decay or gum disease."}/>,
+    <Clause content={"Treatment usually lasts from 12 months to 2 and a half years, and visits to the orthodontist are needed every 4 to 10 weeks."}/>
+]
+
+export const filling = [
+    <Clause content={"The purpose of orthodontic treatment is to make the best of your teeth."}/>,
+    <Clause content={"This includes straightening your teeth so you're able to care for your teeth and gums more easily, and improving your bite so you can eat more comfortably. And your smile will benefit, too."}/>,
+    <Clause content={"Treatment almost always involves using braces to straighten crooked, crowded or protruding teeth, close gaps between teeth, and correct the bite so the top and bottom teeth meet when the mouth is closed."}/>,
+    <Clause content={"You'll need to have healthy teeth and gums before you can have a brace fitted."}/>,
+    <Clause content={"This is because you must be able to keep your teeth and your brace very clean while you're wearing it to avoid getting tooth decay or gum disease."}/>,
+    <Clause content={"Treatment usually lasts from 12 months to 2 and a half years, and visits to the orthodontist are needed every 4 to 10 weeks."}/>
+]
