e871d926932b9b88c61e595c03e649b442245e4d
codeyourwaytofreedom <can.kolej@gmail.com>
2023-01-25 19:01:44 +0300

    Image slider finished.
    Works perfectly irrespective of number of images to...
    be displayed.
    
    Amazing slider.
    
    Can go back to desk page to finalize design.
    
    Great progress today.
    
    Keep going.
    
    Remember why you started.

--- components/home.tsx
+++ components/home.tsx
@@ -11,10 +11,28 @@ import happy1 from "../public/happy1.jpg";
 import happy2 from "../public/happy2.jpg";
 import happy3 from "../public/happy3.jpg";
 import happy4 from "../public/happy4.jpg";
+import happy5 from "../public/happy5.jpg";
+import happy6 from "../public/happy6.jpg";
+import happy7 from "../public/happy7.jpg";
+import happy8 from "../public/happy8.jpg";
 
 const Home_p = () => {
     const [modal, setModal] = useState(false)
     const [service, setService] = useState(braces)
+    const restart = [happy1,happy2,happy3,happy4, happy5, happy6, happy7, happy8]
+    const [smiles,setSmiles] = useState([happy1,happy2,happy3,happy4, happy5, happy6, happy7, happy8])
+
+    setTimeout(() => {
+        let right = smiles.slice(2, 3);
+        let rest = smiles.slice(1,smiles.length)
+        if(smiles.length>3)
+        {
+            setSmiles(smiles.filter(i => i !== smiles[2]))
+        }
+        else{
+            setSmiles(restart)
+        }
+    }, 9000);
 
     return (
         <div className={h.home}>
@@ -47,18 +65,12 @@ const Home_p = () => {
             </div>
             <Chat modal={modal}/>
             <div className={h.home_happy}>
-                <div className={h.home_happy_left} style={{backgroundImage: `url(${happy3.src})`}}>
-
+                <div className={h.home_happy_right} style={{backgroundImage: `url(${smiles[0].src})`}}>
                 </div>
-                <div className={h.home_happy_central} style={{backgroundImage: `url(${happy1.src})`}}>
-
+                <div className={h.home_happy_central} style={{backgroundImage: `url(${smiles[1].src})`}}>
                 </div>
-                <div className={h.home_happy_right} style={{backgroundImage: `url(${happy2.src})`}}>
-
+                <div className={h.home_happy_left} style={{backgroundImage: `url(${smiles[2].src})`}}>
                 </div>
-{/*                 <div className={h.home_happy_right2} style={{backgroundImage: `url(${happy4.src})`}}>
-
-                </div> */}
             </div>
         </div>
       );
